e99fe87cf86f281fcc2400b4ce756b166f4951a1
Neena Ugail <nhu21@bath.ac.uk>
2020-07-17 18:17:39 +0100

    start

---
+++ compoundInterestCalculatorNoGUI.py
@@ -0,0 +1,13 @@
+
+initialAmount = int(input("Initial amount: "))
+interestRate = int(input("Interest rate (%): "))
+
+years = int(input("How many years: "))
+months = int(input("How many months: "))
+
+regular = input("Regular deposit/withdrawal? ")
+
+if regular == "yes":
+    regularAmount = int(input("Enter the regular amount: "))
+
+
