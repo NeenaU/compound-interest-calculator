7a00cd1680c64c7ce5da46cd623b74a6abd50d50
Neena Ugail <nhu21@bath.ac.uk>
2020-08-02 14:54:55 +0100

    initial placement of widgets

--- compoundInterestCalculator.py
+++ compoundInterestCalculator.py
@@ -7,38 +7,43 @@ class interestCalculator():
         master.title = ("Compound Interest Calculator")
 
         #Title
-        self.titleLabel = tk.Label(text="Compound Interest Calculator")
+        self.titleLabel = tk.Label(text="Compound Interest Calculator", font=("Times",16)).grid(sticky='W',columnspan=15,pady=10)
 
         #Options
-        self.initialAmountLabel = tk.Label(text="Initial Amount (£)")
-        self.initialAmountEntry = tk.Entry(master, width=8)
+        self.initialAmountLabel = tk.Label(text="Initial Amount").grid(sticky='W')
+        self.initialAmountSign = tk.Label(text="£").grid(sticky='W',row=2,column=0)
+        self.initialAmountEntry = tk.Entry(master, width=8).grid(row=2,column=1,sticky='W')
 
-        self.calcPeriodLabel = tk.Label(text="Calculation Period")
-        self.yearsLabel = tk.Label(text="years")
-        self.yearsEntry = tk.Entry(master, width=3)
-        self.monthsLabel = tk.Label(text="months")
-        self.monthsEntry = tk.Entry(master, width=3)
+        self.interestRateLabel = tk.Label(text="Yearly Interest Rate").grid(sticky='W')
+        self.interestRateEntry = tk.Entry(master, width=3).grid(sticky='W')
+        self.interestRateSign = tk.Label(text="%").grid(row=4,column=1,sticky='W')
+
+        self.calcPeriodLabel = tk.Label(text="Calculation Period").grid(sticky='W')
+        self.yearsEntry = tk.Entry(master, width=3).grid(sticky='W')
+        self.yearsLabel = tk.Label(text="years").grid(sticky='W')
+        self.monthsEntry = tk.Entry(master, width=3).grid(sticky='W')
+        self.monthsLabel = tk.Label(text="months").grid(sticky='W')
         
-        self.compoundIntervalLabel = tk.Label(text="Compound Interval")
+        self.compoundIntervalLabel = tk.Label(text="Compound Interval").grid(sticky='W')
         self.compoundDefaultVal = tk.StringVar(master)
         self.compoundDefaultVal.set("yearly")
-        self.compoundchoice = tk.OptionMenu(master, self.compoundDefaultVal, "monthly", "yearly")
+        self.compoundchoice = tk.OptionMenu(master, self.compoundDefaultVal, "monthly", "yearly").grid(sticky='W')
 
-        self.regularAmountLabel = tk.Label(text="Regular deposit/withdrawal (£)")
+        self.regularAmountLabel = tk.Label(text="Regular deposit/withdrawal (£)").grid(sticky='W')
         self.regularAmountDefaultVal = tk.StringVar(master)
         self.regularAmountDefaultVal.set("no")
-        self.regularAmountChoice = tk.OptionMenu(master, self.regularAmountDefaultVal, "yes", "no")
-        self.regularAmountEntry = tk.Entry(master, width=8)
+        self.regularAmountChoice = tk.OptionMenu(master, self.regularAmountDefaultVal, "yes", "no").grid(sticky='W')
+        self.regularAmountEntry = tk.Entry(master, width=8).grid(sticky='W')
 
-        self.increaseDepositsLabel = tk.Label(text="Increase deposits in line with annual inflation?")
+        self.increaseDepositsLabel = tk.Label(text="Increase deposits in line with annual inflation?").grid(sticky='W')
         self.increaseDepositsDefaultVal = tk.StringVar(master)
         self.increaseDepositsDefaultVal.set("no")
-        self.increaseDepositsChoice = tk.OptionMenu(master, self.increaseDepositsDefaultVal, "yes", "no")
-        self.annualInflationLabel = tk.Label(text="Annual inflation rate (%)")
-        self.annualInflationEntry = tk.Entry(master, width=3)
+        self.increaseDepositsChoice = tk.OptionMenu(master, self.increaseDepositsDefaultVal, "yes", "no").grid(sticky='W')
+        self.annualInflationLabel = tk.Label(text="Annual inflation rate (%)").grid(sticky='W')
+        self.annualInflationEntry = tk.Entry(master, width=3).grid(sticky='W')
 
-        self.calculateButton = tk.Button(text="Calculate", command=self.calculateResult)
-        self.resultLabel = tk.Label(master)
+        self.calculateButton = tk.Button(text="Calculate", command=self.calculateResult).grid(sticky='W')
+        self.resultLabel = tk.Label(master, text="").grid(sticky='W')
         self.calculateAgainButton= tk.Button(text="Calculate Again", command=self.reset)
 
     def calculateResult(self):
@@ -51,11 +56,12 @@ class interestCalculator():
         print("C")
 
     def reset(self):
+        grid()
         print("D")
 
 def main():
     window = tk.Tk()
-    window.geometry("400x275")
+    window.geometry("300x500")
     #window.configure(bg='#aaabb8')  #window background colour
     generator = interestCalculator(window)
     window.mainloop()
